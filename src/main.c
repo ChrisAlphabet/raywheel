44e993445dbac5080304530ae87a2913c5e5fed0
Chris <c.kolodziej17@gmail.com>
2026-03-02 23:09:52 +1100

    Add a quick proof of concept of dragging a file for user input

--- src/main.c
+++ src/main.c
@@ -19,6 +19,7 @@ static constexpr Vector2 CENTER = {WIDTH/2, HEIGHT/2};
 static constexpr float CIRCLE_RADIUS = { 250.0f };
 static constexpr int banner_font_size = { 50 };
 static const char banner[] = "PRAISE BE TO THE WHEEL!";
+static const char drag_drop_banner[] = "DRAG AND DROP SOULS TO BE JUDGED!";
 
 // triangle constants
 static constexpr int TRI_SIZE = { 50 };
@@ -63,21 +64,15 @@ bool angleInRange(int target, int lower, int upper)
         else return (target >= lower || target <= upper);
 }
 
-int main(void)
+void load_wheel(Wheel * wheel, const char * fname)
 {
-        // ALL HAIL THE WHEEL!
-        Wheel wheel;
-        wheel.spin = true;
-
-        // open the file that contains the player data
-        // expected data format (but doesnt check yet): name,weeks
-        FILE * f = fopen("src/judgement.csv", "r");
+        FILE * f = fopen(fname, "r");
         if (!f) 
         {
                 perror("Error opening 'judgement.csv'");
-                return 1;
+                return;
         } 
-        
+
         char line[MAX_LINE];
         int count = { 0 };
 
@@ -86,18 +81,25 @@ int main(void)
         // process the csv data to populate the player data
         while(fgets(line, sizeof(line), f) && (count < MAX_PEOPLE))
         {
-                sscanf(line, "%24[^,],%d", wheel.players[count].name, &wheel.players[count].week); 
-                wheel.players[count].color = randomColor();
-                wheel.players[count].active = true;
+                sscanf(line, "%24[^,],%d", wheel->players[count].name, &wheel->players[count].week); 
+                wheel->players[count].color = randomColor();
+                wheel->players[count].active = true;
                 count++;
         }
 
         // set the number of players and count all players as active
-        wheel.N_players = count;
-        wheel.N_active = wheel.N_players;
+        wheel->N_players = count;
+        wheel->N_active = wheel->N_players;
 
         // close the csv data
         fclose(f);
+}
+
+int main(void)
+{
+        // ALL HAIL THE WHEEL!
+        Wheel wheel;
+        wheel.spin = true;
 
         // variable for rendering wheel
         Vector2 text_pos = {0, 0};
@@ -108,6 +110,7 @@ int main(void)
         float text_angle = 0.0f;
         int i_selected;
         int i_active;
+        bool data_loaded = false;
 
         // setup the raylib window
         SetTargetFPS(FPS);
@@ -116,6 +119,9 @@ int main(void)
         // center the banner message above the wheel
         int banner_width = MeasureText(banner, banner_font_size);
         Vector2 banner_pos = { CENTER.x - (banner_width / 2), CENTER.y - CIRCLE_RADIUS - (2 * banner_font_size) };
+
+        int drag_drop_width = MeasureText(drag_drop_banner, banner_font_size);
+        Vector2 drag_drop_pos = { CENTER.x - (drag_drop_width / 2), CENTER.y };
 
         // raylib loop
         while (!WindowShouldClose())
@@ -126,84 +132,100 @@ int main(void)
                 ClearBackground(RAYWHITE);
                 
                 DrawText("PRAISE BE TO THE WHEEL!", banner_pos.x, banner_pos.y, banner_font_size, BLACK);
-                
-                // control the rotation speed
-                rotation += friction * GetFrameTime(); 
-                rotation = fmodf(rotation, 360.0f);
 
-                // set the sector size based on the number of active players
-                sector_size = 360.0f / wheel.N_active;
-
-                // reset the index used to offset the starting angle of each player
-                i_active = 0;
-
-                for (int i = 0; i < wheel.N_players; i++)
+                // load player data
+                if (!data_loaded)
                 {
-                        // skip inactive players that have been judged worthy
-                        if (!wheel.players[i].active) continue;        
+                        DrawText(drag_drop_banner, drag_drop_pos.x, drag_drop_pos.y, banner_font_size, BLACK);
 
-                        // calculate the start and end angles
-                        wheel.players[i].start_angle = i_active * sector_size + rotation; 
-                        wheel.players[i].end_angle = wheel.players[i].start_angle + sector_size; 
-                        
-                        // mark the current player selected/worthy if the game is still active
-                        if (angleInRange(0.0f, wheel.players[i].start_angle, wheel.players[i].end_angle) && wheel.spin) 
-                                i_selected = i;
-                        
-                        // calculate the player text location
-                        text_angle = wheel.players[i].start_angle + (sector_size / 2);
-                        text_pos.x = text_radius * cosf(DEG2RAD * text_angle);
-                        text_pos.y = text_radius * sinf(DEG2RAD * text_angle);
-                        
-                        // render the circle sector and text
-                        DrawCircleSector(CENTER, CIRCLE_RADIUS, wheel.players[i].start_angle, wheel.players[i].end_angle, 100, wheel.players[i].color);
-                        DrawText(wheel.players[i].name, text_pos.x + CENTER.x, text_pos.y + CENTER.y, 20, BLACK); 
-                        
-                        // increment the active counter
-                        i_active++;
-                }
-
-                if (!wheel.spin)
-                {
-                        // show winner
-                        char winner_banner[51];
-                        snprintf(winner_banner, sizeof(winner_banner), "%s has been judged unworthy!", wheel.players[wheel.i_winner].name);
-                        int winner_width = MeasureText(winner_banner, banner_font_size);
-                        Vector2 banner_pos = { CENTER.x - (winner_width / 2), CENTER.y + CIRCLE_RADIUS + banner_font_size };
-                        DrawText(winner_banner, banner_pos.x, banner_pos.y, banner_font_size, wheel.players[wheel.i_winner].color);
-                }
-                
-                // if the wheel has stopped
-                if (friction == 0) 
-                { 
-                        if (wheel.N_active > 1) 
+                        if (IsFileDropped())
                         {
-                                // remove the selected player and restart the wheel
-                                wheel.players[i_selected].active = false;
-                                wheel.N_active -= 1;
-                                friction = GetRandomValue(100, 255);
-                        }
-                        else if (wheel.N_active == 1)
-                        {
-                                // the game is over and we have a winner!
-                                wheel.spin = false;
-                                wheel.i_winner = i_selected;
-                                friction = 60;
-                        }
-                        else
-                        {
-                                // do nothing
+                                FilePathList dropped_files = LoadDroppedFiles();
+                                load_wheel(&wheel, dropped_files.paths[0]);
+                                UnloadDroppedFiles(dropped_files);
+                                data_loaded = true;
                         }
                 }
-                else 
+                else
                 {
-                        // slow the wheel
-                        friction -= 1;
-                }
+                        // control the rotation speed
+                        rotation += friction * GetFrameTime(); 
+                        rotation = fmodf(rotation, 360.0f);
 
-                // render the 0 degree wheel selection marker (this took longer than I care to admit)
-                DrawTriangle(TRI_V1, TRI_V2, TRI_V3, BLACK);
-		EndDrawing();
+                        // set the sector size based on the number of active players
+                        sector_size = 360.0f / wheel.N_active;
+
+                        // reset the index used to offset the starting angle of each player
+                        i_active = 0;
+
+                        for (int i = 0; i < wheel.N_players; i++)
+                        {
+                                // skip inactive players that have been judged worthy
+                                if (!wheel.players[i].active) continue;        
+
+                                // calculate the start and end angles
+                                wheel.players[i].start_angle = i_active * sector_size + rotation; 
+                                wheel.players[i].end_angle = wheel.players[i].start_angle + sector_size; 
+                                
+                                // mark the current player selected/worthy if the game is still active
+                                if (angleInRange(0.0f, wheel.players[i].start_angle, wheel.players[i].end_angle) && wheel.spin) 
+                                        i_selected = i;
+                                
+                                // calculate the player text location
+                                text_angle = wheel.players[i].start_angle + (sector_size / 2);
+                                text_pos.x = text_radius * cosf(DEG2RAD * text_angle);
+                                text_pos.y = text_radius * sinf(DEG2RAD * text_angle);
+                                
+                                // render the circle sector and text
+                                DrawCircleSector(CENTER, CIRCLE_RADIUS, wheel.players[i].start_angle, wheel.players[i].end_angle, 100, wheel.players[i].color);
+                                DrawText(wheel.players[i].name, text_pos.x + CENTER.x, text_pos.y + CENTER.y, 20, BLACK); 
+                                
+                                // increment the active counter
+                                i_active++;
+                        }
+
+                        if (!wheel.spin)
+                        {
+                                // show winner
+                                char winner_banner[51];
+                                snprintf(winner_banner, sizeof(winner_banner), "%s has been judged unworthy!", wheel.players[wheel.i_winner].name);
+                                int winner_width = MeasureText(winner_banner, banner_font_size);
+                                Vector2 banner_pos = { CENTER.x - (winner_width / 2), CENTER.y + CIRCLE_RADIUS + banner_font_size };
+                                DrawText(winner_banner, banner_pos.x, banner_pos.y, banner_font_size, wheel.players[wheel.i_winner].color);
+                        }
+                        
+                        // if the wheel has stopped
+                        if (friction == 0) 
+                        { 
+                                if (wheel.N_active > 1) 
+                                {
+                                        // remove the selected player and restart the wheel
+                                        wheel.players[i_selected].active = false;
+                                        wheel.N_active -= 1;
+                                        friction = GetRandomValue(100, 255);
+                                }
+                                else if (wheel.N_active == 1)
+                                {
+                                        // the game is over and we have a winner!
+                                        wheel.spin = false;
+                                        wheel.i_winner = i_selected;
+                                        friction = 60;
+                                }
+                                else
+                                {
+                                        // do nothing
+                                }
+                        }
+                        else 
+                        {
+                                // slow the wheel
+                                friction -= 1;
+                        }
+
+                        // render the 0 degree wheel selection marker (this took longer than I care to admit)
+                        DrawTriangle(TRI_V1, TRI_V2, TRI_V3, BLACK);
+                }
+                EndDrawing();
 	}
 
 	CloseWindow();
